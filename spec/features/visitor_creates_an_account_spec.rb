2bcb9a697b05afd8124f2319a517e56d64389ec7
njgheorghita <njgheorghita@email.wm.edu>
2017-02-07 19:36:22 -0700

    write test

--- spec/features/visitor_creates_an_account_spec.rb
+++ spec/features/visitor_creates_an_account_spec.rb
@@ -2,12 +2,18 @@ require "rails_helper"
 
 describe Feature do 
   describe "A visitor" do 
-    it "can register as a user" do 
+    it "can register as a user with correct field inputs" do 
       # when I visit the homepage
       visit root_path
       # and I click 'Create Account'
       click_on "Create Account"
+      expect(current_path).to eq(new_session_path)
       # and I fill out my information (see user model requirements) 
+      fill_in "First Name", with: "First Name"
+      fill_in "Second Name", with: "Second Name"
+      fill_in "Email", with: "Email"
+      fill_in "Phone", with: "Phone"
+      fill_in "Password", with: "Password"
       # and I create and confirm a password (two rows)
       # and I click 'Create Account'
       click_on "Create Account"
@@ -17,9 +23,16 @@ describe Feature do
       # and I should not see a link to 'Create Account'
       expect(page).not_to have_content("Create Account")
       # and I should see my information
-      expect(page).to have_content("Name")
+      expect(page).to have_content("First Name")
+      expect(page).to have_content("Second Name")
       expect(page).to have_content("Email")
       expect(page).to have_content("Phone")
+    end
+
+    it "cannot register as a user without correct field inputs" do 
+      visit root_path
+      click_on "Create Account" 
+      expect(current_path).to eq(new_session_path)
     end
   end
 end
