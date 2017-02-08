779d93eb7dc97db0372aea89e988779a4dee80d8
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-02-08 15:02:45 -0700

    Night migration and schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170208211308) do
+ActiveRecord::Schema.define(version: 20170208215921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,16 @@ ActiveRecord::Schema.define(version: 20170208211308) do
     t.datetime "updated_at",     null: false
     t.integer  "user_id"
     t.index ["user_id"], name: "index_couches_on_user_id", using: :btree
+  end
+
+  create_table "nights", force: :cascade do |t|
+    t.date     "date"
+    t.integer  "couch_id"
+    t.integer  "reservation_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.index ["couch_id"], name: "index_nights_on_couch_id", using: :btree
+    t.index ["reservation_id"], name: "index_nights_on_reservation_id", using: :btree
   end
 
   create_table "reservations", force: :cascade do |t|
@@ -47,5 +57,7 @@ ActiveRecord::Schema.define(version: 20170208211308) do
   end
 
   add_foreign_key "couches", "users"
+  add_foreign_key "nights", "couches"
+  add_foreign_key "nights", "reservations"
   add_foreign_key "reservations", "users"
 end
