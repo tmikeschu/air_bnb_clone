7126d663d12106c38c7839f74c38f2df94b5fd4c
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-02-08 15:33:07 -0700

    Label statuses for Reservation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,6 @@
 class Reservation < ApplicationRecord
   validates_presence_of :status
   belongs_to :traveler, class_name: "User", foreign_key: :user_id
+
+  enum status: %w(pending confirmed canceled)
 end
