0f05dab3a8c7a84313dd754aec14fec41aa81b49
Brett Rivers <riverswb@gmail.com>
2017-02-07 18:34:19 -0700

    Removes email presence validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  validates :email, presence: :true
   validates :email, uniqueness: :true
   has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,16 +2,11 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   context "validations" do
-    it "requires an email" do
-      invalid_user = User.create(email: nil)
-
-      expect(invalid_user).to be_invalid
-    end
-
     it "email must be unique" do
       user = create(:user)
       invalid_user = User.create(email: user.email)
 
+      expect(user).to be_valid
       expect(invalid_user).to be_invalid
     end
   end
