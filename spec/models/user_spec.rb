fe46a4fb338af36de7aecaa71ca39a7b1047394b
Brett Rivers <riverswb@gmail.com>
2017-02-08 07:35:45 -0700

    Refactors user model test to use shoulda matchers

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,27 +2,9 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   context "validations" do
-    it "email must be unique" do
-      user = create(:user)
-      invalid_user = User.create(email: user.email)
-
-      expect(user).to be_valid
-      expect(invalid_user).to be_invalid
-    end
-
-    it "phone number must be present" do
-      invalid_user = User.create(phone_number: nil)
-
-      expect(invalid_user).to be_invalid
-    end
-
-    it "phone number must be unique" do
-      user = create(:user)
-      invalid_user = User.create(phone_number: user.phone_number)
-
-      expect(user).to be_valid
-      expect(invalid_user).to be_invalid
-    end
+    it { should validate_uniqueness_of :email }
+    it { should validate_presence_of :phone_number}
+    it { should validate_uniqueness_of :phone_number}
   end
 
   context "relationships" do
