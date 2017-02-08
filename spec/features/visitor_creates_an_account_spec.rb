c179d3e8da416521c46bfd6b096758d8297bfcf6
njgheorghita <njgheorghita@email.wm.edu>
2017-02-07 23:32:02 -0700

    remove comments from tests

--- spec/features/visitor_creates_an_account_spec.rb
+++ spec/features/visitor_creates_an_account_spec.rb
@@ -3,26 +3,19 @@ require "rails_helper"
 describe User, type: :feature do 
   describe "A visitor" do 
     it "can register as a user with correct field inputs" do 
-      # when I visit the homepage
       visit root_path
-      # and I click 'Create Account'
       click_on "Create Account"
       expect(current_path).to eq(new_user_path)
-      # and I fill out my information (see user model requirements) 
+
       fill_in "First name", with: "First Name"
       fill_in "Last name", with: "Last Name"
       fill_in "Email", with: "Email"
       fill_in "Phone number", with: "Phone"
       fill_in "Password", with: "Password"
-      # and I create and confirm a password (two rows)
-      # and I click 'Create Account'
+      
       click_on "Sign Up"
 
-      # then I should be on my account page
-      # expect(page).to eq user_path(1)
-      # and I should not see a link to 'Create Account'
       expect(page).not_to have_content("Create Account")
-      # and I should see my information
       expect(page).to have_content("First Name")
       expect(page).to have_content("Last Name")
       expect(page).to have_content("Email")
