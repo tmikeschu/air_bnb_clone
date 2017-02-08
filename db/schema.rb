7a4dba7ebea46a811a243c5ae74bdf311de48660
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-02-08 14:53:13 -0700

    Reservation database migration and schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170208140001) do
+ActiveRecord::Schema.define(version: 20170208211308) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,14 @@ ActiveRecord::Schema.define(version: 20170208140001) do
     t.index ["user_id"], name: "index_couches_on_user_id", using: :btree
   end
 
+  create_table "reservations", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_reservations_on_user_id", using: :btree
+  end
+
   create_table "users", force: :cascade do |t|
     t.text     "email"
     t.text     "first_name"
@@ -39,4 +47,5 @@ ActiveRecord::Schema.define(version: 20170208140001) do
   end
 
   add_foreign_key "couches", "users"
+  add_foreign_key "reservations", "users"
 end
