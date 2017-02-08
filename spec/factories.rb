30bbc2f91b7b6eb150bba41c0c27988007edc7bd
Brett Rivers <riverswb@gmail.com>
2017-02-08 13:53:00 -0700

    Fixes some factory girl issues

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,23 +1,34 @@
 FactoryGirl.define do
 
-  sequence(:email) { |n| "#{n}@test.com" }
-  sequence(:phone_number) { |n| "#{n}123456789"}
-
   factory :user do
-    first_name   Faker::Name.first_name
-    last_name    Faker::Name.last_name
-    email
-    password     Faker::Internet.password
-    phone_number
+    first_name   { Faker::Name.first_name }
+    last_name    { Faker::Name.last_name }
+    email        { Faker::Internet.email }
+    password     { Faker::Internet.password }
+    phone_number { Faker::PhoneNumber.unique.phone_number }
   end
 
   factory :couch do
-    name           Faker::Lorem.word
-    description    Faker::Hipster.sentence
-    street_address Faker::Address.street_address
-    city           Faker::Address.city
-    state          Faker::Address.state
-    zipcode        Faker::Address.zip_code
+    cities = [{
+                city: "Chicago",
+                state: "IL",
+                zipcode: "12345"},
+              { city: "New Orleans",
+                state: "LA",
+                zipcode: "70112"},
+              { city: "Chicago",
+                state: "IL",
+                zipcode: "12345"}
+              ]
+
+    this_city = cities.sample
+
+    name           { Faker::Lorem.word }
+    description    { Faker::Hipster.sentence }
+    street_address { Faker::Address.street_address }
+    city           this_city[:city]
+    state          this_city[:state]
+    zipcode        this_city[:zipcode]
     user
   end
 end
