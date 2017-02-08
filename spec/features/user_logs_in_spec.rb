f19c6e22764d6b64c8b578ddd81ad6f2906eb74e
njgheorghita <njgheorghita@email.wm.edu>
2017-02-07 22:30:57 -0700

    write user logs in test

---
+++ spec/features/user_logs_in_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe User, type: :feature do
+  describe "As a registered user" do 
+    it "I can login" do 
+      user = create(:user)
+
+      visit root_path
+      click_on "Log In"
+
+      fill_in "Email", with: user.Email
+      fill_in "Password", with: user.Password
+
+      expect(current_path).to eq(user_path(user.id))
+      expect(page).to have_content(user.first_name)
+      expect(page).to have_content(user.last_name)
+      expect(page).to have_content(user.phone_number)
+      expect(page).to have_content("Sign Out")
+      expect(page).not_to have_content("Log In")
+      expect(page).not_to have_content("Create Account")
+
+    end
+  end
+end
