083faba3efbfebfd33435572631fdd661d45c7cd
njgheorghita <njgheorghita@email.wm.edu>
2017-02-08 10:15:17 -0700

    Add current_user helper method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
-      session[:current_user] = user.id
+      session[:current_user_id] = user.id
       redirect_to user_path(user)
     else
       flash[:danger] = "Unsuccessful login attempt"
