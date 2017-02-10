51a5d4538fd55a5c6d43187fdfc8a780cdf9fcf2
Brett Rivers <riverswb@gmail.com>
2017-02-09 17:06:32 -0700

    Adds creates array of dates from user input

--- app/controllers/nights_controller.rb
+++ app/controllers/nights_controller.rb
@@ -5,7 +5,12 @@ class NightsController < ApplicationController
   end
 
   def create
-    night = Night.new(night_params)
+    # night = Night.new(night_params)
+    couch = Couch.find(params[:couch_id])
+    night_params.each do |night|
+      couch.nights.create!(date: night)
+    end
+    byebug
     if night.save
       flash[:success] = "Made night(s) available!"
       redirect_to couch_path(couch)
@@ -16,8 +21,12 @@ class NightsController < ApplicationController
   end
 
   private
-
     def night_params
       params.permit("First Night", "Last Night")
+      x = params["First Night"].split("/").join(" ")
+      y = params["Last Night"].split("/").join(" ")
+      first_date = Date.strptime(x, '%m %d %Y')
+      last_date = Date.strptime(y, '%m %d %Y')
+      all_dates = (first_date..last_date).to_a
     end
 end
