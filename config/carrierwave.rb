f8d854d90b37ee12145a5d3f1a5cf35d4b8dbfb1
Mike Schutte <tmikeschutte@gmail.com>
2017-02-10 11:49:36 -0700

    Add carrierwave configuration

---
+++ config/carrierwave.rb
@@ -0,0 +1,8 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    provider: 'Google',
+    google_storage_access_key_id: ENV['GOOGLE_STORAGE_ACCESS_KEY_ID'],
+    google_storage_secret_access_key: ENV['GOOGLE_STORAGE_SECRET_ACCESS_KEY']
+  }
+  config.fog_directory = 'photo-of-the-day'
+end
