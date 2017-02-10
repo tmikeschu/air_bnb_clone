71d2531e61551440e3297cf4b2867cabbc15b7fe
Jesse Shipley <jesse.radack@gmail.com>
2017-02-10 10:34:27 -0700

    [WIP] actual couch adding

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :sessions,  only: [:new]
   resources :users,     only: [:new, :create, :show] do
+    resources :couches
     scope module: :users do
       resources :reservations, only: [:index]
     end
