c39ab34bfd7695d79825b02b9da83f9b7e7e0bc5
Mike Schutte <tmikeschutte@gmail.com>
2017-02-10 18:01:36 -0700

    Move carrierwave.rb to initializers

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,5 +4,6 @@ CarrierWave.configure do |config|
     google_storage_access_key_id: ENV['GOOGLE_STORAGE_ACCESS_KEY_ID'],
     google_storage_secret_access_key: ENV['GOOGLE_STORAGE_SECRET_ACCESS_KEY']
   }
+  config.storage = :fog
   config.fog_directory = 'airbnb-clone'
 end
