84a950bdec9ab84cb656fc6e503261a1057d60be
njgheorghita <njgheorghita@email.wm.edu>
2017-02-12 16:38:30 -0700

    Create couch listing date picker

--- app/assets/javascripts/dateselect.js
+++ app/assets/javascripts/dateselect.js
@@ -1,4 +1,15 @@
+var array = ["2017-02-20","2017-02-21","2017-02-22" ]
+
 $(document).on('ready page:load', function() {
   $('#Check_In').datepicker();
   $('#Check_Out').datepicker();
+  $('#Couch_Listing').datepicker({
+    beforeShowDay: function(date){
+      var string = jQuery.datepicker.formatDate('yy-mm-dd', date);
+      return [ array.indexOf(string) == -1 ]
+    },
+
+    numberOfMonths: 3,
+    showButtonPanel: true
+  });
 });
--- app/models/night.rb
+++ app/models/night.rb
@@ -1,7 +1,7 @@
 class Night < ApplicationRecord
   validates_presence_of :date
   belongs_to :couch
-  belongs_to :reservation
+  belongs_to :reservation, optional: true
 
   scope :all_for_couch, -> (couch) { where(couch: couch) }
   scope :between_check_in_check_out, -> (check_in, check_out) {
