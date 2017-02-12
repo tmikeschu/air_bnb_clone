1bc938321677d4b6439cfd5d5979ebe795921d55
Brett Rivers <riverswb@gmail.com>
2017-02-12 15:01:33 -0700

    Adds sad path test, dates in wrong order.

--- spec/features/host_makes_couch_available_spec.rb
+++ spec/features/host_makes_couch_available_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-RSpec.feature "Host", type: :feature do
+RSpec.feature "Nights", type: :feature do
   let!(:couch) {create(:couch)}
 
-  it "lists a couch" do
+  scenario "host lists a couch" do
     host = couch.host
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(host)
 
@@ -28,27 +28,55 @@ RSpec.feature "Host", type: :feature do
     expect(page).to have_content(last_format)
   end
 
-  it "SAD path" do
-    host = couch.host
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(host)
+  describe "SAD path" do
+    scenario "date can't be in past" do
+      host = couch.host
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(host)
 
-    visit user_couch_path(host, couch)
+      visit user_couch_path(host, couch)
 
-    click_on "Add Availability"
+      click_on "Add Availability"
 
-    yesterday = Date.yesterday.strftime('%m/%d/%Y')
-    next_month = Date.current.next_month.strftime('%m/%d/%Y')
+      yesterday = Date.yesterday.strftime('%m/%d/%Y')
+      next_month = Date.current.next_month.strftime('%m/%d/%Y')
 
-    fill_in "First Night", with: yesterday
-    fill_in "Last Night", with: next_month
-    click_on "Make Available"
+      fill_in "First Night", with: yesterday
+      fill_in "Last Night", with: next_month
+      click_on "Make Available"
 
-    last = next_month.split("/")
-    last_format = "#{last[2]}-#{last[0]}-#{last[1]}"
+      last = next_month.split("/")
+      last_format = "#{last[2]}-#{last[0]}-#{last[1]}"
 
 
-    expect(current_path).to eq(new_couch_night_path(couch))
-    expect(page).to have_content("Date can't be in the past")
-    expect(page).to_not have_content(last_format)
+      expect(current_path).to eq(new_couch_night_path(couch))
+      expect(page).to have_content("Date can't be in the past")
+      expect(page).to_not have_content(last_format)
+    end
+
+    scenario "end date before start date" do
+      host = couch.host
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(host)
+
+      visit user_couch_path(host, couch)
+
+      click_on "Add Availability"
+
+      tomorrow = Date.tomorrow.strftime('%m/%d/%Y')
+      next_month = Date.current.next_month.strftime('%m/%d/%Y')
+
+      fill_in "First Night", with: next_month
+      fill_in "Last Night", with: tomorrow
+      click_on "Make Available"
+
+      last = next_month.split("/")
+      last_format = "#{last[2]}-#{last[0]}-#{last[1]}"
+
+
+      expect(current_path).to eq(new_couch_night_path(couch))
+      expect(page).to have_content("First night must be after last night")
+      expect(page).to_not have_content(last_format)
+    end
+
+
   end
 end
