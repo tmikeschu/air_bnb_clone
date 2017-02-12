6de045b6aa67ea93564109f17bc139cd9f65f3d6
Mike Schutte <tmikeschutte@gmail.com>
2017-02-12 11:02:49 -0700

    Address PR comments
    Add sad path logic to create couch photo
    
    Add image format label

--- app/controllers/couches/photos_controller.rb
+++ app/controllers/couches/photos_controller.rb
@@ -4,11 +4,13 @@ class Couches::PhotosController < ApplicationController
   end
 
   def create
-
     couch = Couch.find(params["couch_id"])
     @photo = couch.photos.new(photo_params)
     if @photo.save!
       redirect_to user_couch_path(current_user, couch)
+    else
+      flash[:danger] = @photo.errors.full_messages.join(", ")
+      redirect_to new_couch_photo_path
     end
   end
 
