22406c33beaddf18bfb79a171f5ceb027648e4a0
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-02-15 16:46:45 -0700

    Scope to order messages, newest first

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,8 @@ class Message < ApplicationRecord
   belongs_to :reservation
   belongs_to :author, class_name: "User", foreign_key: :user_id
 
+  scope :by_created_at, -> { order(created_at: :desc) }
+
   def author_name
     "#{author.first_name.capitalize} #{author.last_name.first.capitalize}."
   end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -10,6 +10,16 @@ describe Message do
     it { is_expected.to belong_to(:author).class_name("User") }
   end
 
+  describe ".by_created_at" do
+    let!(:message_1) { create(:message, created_at: Date.current) }
+    let!(:message_2) { create(:message, created_at: Date.current + 1.day) }
+    let!(:message_3) { create(:message, created_at: Date.current - 1.day) }
+
+    it "returns messages in order of the date and time they were created, newest first" do
+      expect(Message.by_created_at).to eq [message_2, message_1, message_3]
+    end
+  end
+
   describe "#author_name" do
     let!(:author) { create(:user, first_name: "Han", last_name: "Solo") }
     let!(:message) { create(:message, author: author) }
