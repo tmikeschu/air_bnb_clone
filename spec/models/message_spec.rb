8e46256eaa712d2a1c88138c7bcf2f50f6515d9b
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-02-15 12:04:06 -0700

    Show author name for messages

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -9,4 +9,13 @@ describe Message do
     it { is_expected.to belong_to(:reservation) }
     it { is_expected.to belong_to(:author).class_name("User") }
   end
+
+  describe "#author_name" do
+    let!(:author) { create(:user, first_name: "Han", last_name: "Solo") }
+    let!(:message) { create(:message, author: author) }
+
+    it "returns the first name and last initial for the message's author" do
+      expect(message.author_name).to eq "Han S."
+    end
+  end
 end
