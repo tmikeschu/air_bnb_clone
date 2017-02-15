40d90a1bd8181b171139abfdd1b841708dfc08b1
Brett Rivers <riverswb@gmail.com>
2017-02-15 15:51:01 -0700

    Ignore password reset test

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -6,8 +6,6 @@ class ConfirmationsController < ApplicationController
     @user = User.find_by(email: params[:confirmations][:email])
     if @user.verification_code == params[:confirmations][:verification_code]
       @user.save!
-      session[:authenticated] = true
-
       flash[:success] = "Verification success!"
       redirect_to edit_password_reset_path(@user)
     else
