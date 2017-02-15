a8f39777d4a3eb5d9d0bbd2b640e0bb96e8beb14
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-02-15 16:46:03 -0700

    Ensure user is logged in to create messages for a reservation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
 
+  def authorize_user
+    render file: "public/404", status: 404 unless current_user
+  end
+
   private
     def current_user
       @current_user ||= User.find(session[:current_user_id]) if session[:current_user_id]
