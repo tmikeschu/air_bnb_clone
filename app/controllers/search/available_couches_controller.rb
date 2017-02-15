ca8ad435474e1a9dc6c1906e2bd923c1a68a6f65
njgheorghita <njgheorghita@email.wm.edu>
2017-02-15 11:53:14 -0700

    Refactor ajax search update
    Condense update functions into index action

--- app/controllers/search/available_couches_controller.rb
+++ app/controllers/search/available_couches_controller.rb
@@ -8,14 +8,7 @@ class Search::AvailableCouchesController < ApplicationController
     gon.available_cities = Couch.available_cities
   end
 
-  def update
-    @search_params = search_params
-    @query = QueryPresenter.present(search_params)
-    gon.available_cities = Couch.available_cities
-  end
-
   private
-
     def search_params
       params.permit("Check In", "Check Out", "Destination")
     end
