0442fde5c213320e30f38bf634f38de096d6ce08
Mike Schutte <tmikeschutte@gmail.com>
2017-02-16 00:43:27 -0700

    Update simplecov filters

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,7 +8,10 @@ require 'rspec/rails'
 require 'capybara/rails'
 require 'vcr'
 require "simplecov"
-SimpleCov.start "rails"
+SimpleCov.start "rails" do
+  add_filter "/app/channels/application_cable" # Ignores any file containing "/vendor/" in its path.
+  add_filter "/app/jobs" # Ignores a specific file.
+end
 
 VCR.configure do |config|
   config.cassette_library_dir = Rails.root.join("spec","fixtures","vcr_cassettes")
