5fde66bb182c1e2eecc347179542e47fa0568266
Mike Schutte <tmikeschutte@gmail.com>
2017-02-15 19:57:25 -0700

    Style host profile page

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -8,4 +8,15 @@ RSpec.describe Profile, type: :model do
   context "relationships" do
     it { should belong_to :user}
   end
+
+  context "methods" do
+    describe "#user_name" do
+      it "returns user's first name and last initial" do
+        profile = create(:profile)
+        user    = profile.user
+        expected = "#{user.first_name.capitalize} #{user.last_name[0]}."
+        expect(profile.user_name).to eq expected
+      end
+    end
+  end
 end
