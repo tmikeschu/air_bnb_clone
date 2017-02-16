6a0cb32c031f7c172ca2f369c07661b829573a0e
Brett Rivers <riverswb@gmail.com>
2017-02-15 17:43:34 -0700

    Fixes test after moving the link to hosts profile

--- spec/features/profiles/traveler_can_view_hosts_profile_spec.rb
+++ spec/features/profiles/traveler_can_view_hosts_profile_spec.rb
@@ -22,11 +22,12 @@ RSpec.feature "Profiles" do
                         "Check Out": tomorrow.to_date_picker_format)
 
       expect(current_path).to eq(search_path)
-      expect(page).to have_content "1 couch in #{couch.city} available starting on #{today.to_date_picker_format}"
+      expect(page).to have_content "1 couch in #{couch.city} available"
 
-      click_link("Host's Profile")
+      click_link("View Couch")
 
-      expect(current_path).to eq(user_profile_path(couch.user_id, profile))
+      click_link("#{profile.user.first_name}")
+      
       expect(page).to have_content(profile.description)
     end
   end
